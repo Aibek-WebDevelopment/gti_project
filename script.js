57a0dd34cd58659c31c6df43ffe637c8a3fd6577
Aibek <ajbeknurzuma6@gmail.com>
2023-09-07 23:12:49 +0600

    add js string

---
+++ script.js
@@ -0,0 +1,11 @@
+// const numberfilms = +prompt("Сколько фильмов вы посмотрели?");
+// console.log(numberfilms);
+
+
+// const personalmovie = {
+//     count: numberfilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
