cfd96427aa73852657da67ccac5d10b48915e3e9
Aibek <ajbeknurzuma6@gmail.com>
2023-09-09 22:51:17 +0600

    54-64 string code

--- script.js
+++ script.js
@@ -51,3 +51,14 @@
 // } else {
 //     console.log("no  fuck!");
 // }
+
+// let result = "";
+// let length = 7;
+
+// for (let i = 1; i < length; i++) {
+//     for (let j = 0; j < i; j++) {
+//         result += "*";
+//     }
+//     result += "\n";
+// }
+// console.log(result);
