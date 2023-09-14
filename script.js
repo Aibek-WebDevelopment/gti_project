4f63d76acb61e779c1e1a26a4a283dce54a62ec7
Aibek <ajbeknurzuma6@gmail.com>
2023-09-14 14:54:09 +0600

    add string 65-108

--- script.js
+++ script.js
@@ -62,3 +62,47 @@
 //     result += "\n";
 // }
 // console.log(result);
+
+// for (let i = 5; i <= 10; i++) {
+//     console.log("Num = " + i);
+// }
+
+
+// for (let i = 20; i >= 10; i--) {
+//     if (i === 13) {
+//         break;
+//     }
+//     console.log("Num = " + i);
+// }
+
+
+// for (let i = 2; i <= 10; i++) {
+//     if (i % 2 == 0) {
+//         console.log("Num = " + i);
+//     }
+// }
+
+
+// let i = 2;
+// while (i <= 16) {
+//     i++;
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+
+// function fifthTask() {
+//     const arrayOfNumbers = [];
+
+//     for (let i = 5; i < 11; i++) {
+//         arrayOfNumbers[i - 5] = i;
+//     }
+
+//     console.log(arrayOfNumbers);
+//     return arrayOfNumbers;
+// }
+
+// fifthTask()
